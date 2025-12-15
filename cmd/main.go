04e32016a78d6d0534ed6a9f67228492ecdde0e6
Eugeny <eugeny.zhidkov@gmail.com>
2025-12-15 18:59:34 +0500

    Full S3 Files Manager implementation

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	defer logger.Sync()
 
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM) // Можно использовать os.Interrupt вместо SIGINT?
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM) // Можно использовать os.Interrupt, а не SIGINT?
 
 	/*os.Interrupt — кроссплатформенная константа. На всех ОС (Linux, macOS, Windows) она означает "прерывание от пользователя" (Ctrl+C).
 	  syscall.SIGINT — низкоуровневая, и на Windows может вести себя иначе.
